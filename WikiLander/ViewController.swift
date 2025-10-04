94a8ec98965e52f25b382bb4285df3bd60fc5435
David Stolarsky <david.stolarsky@gmail.com>
2025-10-04 11:15:09 -0400

    desktop layout on mobile

--- WikiLander/ViewController.swift
+++ WikiLander/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    private var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Configure webview to show desktop layout
+        let configuration = WKWebViewConfiguration()
+        configuration.applicationNameForUserAgent = "Version/15.0 Safari/605.1.15"
+
+        webView = WKWebView(frame: view.bounds, configuration: configuration)
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        // Set custom user agent for desktop layout
+        webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
+
+        view.addSubview(webView)
+
+        // Calculate scale to fit 1920px logical width
+        let screenWidth = UIScreen.main.bounds.width
+        let scale = screenWidth / 1920.0
+        webView.scrollView.minimumZoomScale = scale
+        webView.scrollView.maximumZoomScale = scale
+        webView.scrollView.zoomScale = scale
+
+        // Load Wikipedia
+        if let url = URL(string: "https://en.wikipedia.org/wiki/Main_Page") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
 }
 
