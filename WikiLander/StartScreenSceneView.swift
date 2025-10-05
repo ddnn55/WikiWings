1a844c7406ca8f04e7850bf50b4229ad991766a1
David Stolarsky <david.stolarsky@gmail.com>
2025-10-05 08:57:30 -0400

    enhance start screen with rocket animation and improved positioning

--- WikiLander/StartScreenSceneView.swift
+++ WikiLander/StartScreenSceneView.swift
@@ -9,12 +9,80 @@ import SwiftUI
 
 struct StartScreenSceneView: View {
     var body: some View {
-        VStack {
-            Text("WikiWings")
-                .font(.system(size: 200, weight: .bold))
-                .foregroundColor(.white)
+        ZStack {
+            RocketAnimationView()
+
+            VStack {
+                Text("WikiWings")
+                    .font(.system(size: 200, weight: .bold))
+                    .foregroundColor(.white)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black.opacity(0.5))
     }
+}
+struct Rocket: Identifiable {
+    let id = UUID()
+    var position: CGPoint
+    var fontSize: CGFloat
+    var speed: CGFloat
+}
+
+struct RocketAnimationView: View {
+    @State private var rockets: [Rocket] = []
+    let rocketCount = 8
+    let rocketEmoji = "🚀"
+    let fontSizes: [CGFloat] = [40, 60, 80, 100, 120]
+
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                ForEach(rockets) { rocket in
+                    Text(rocketEmoji)
+                        .font(.system(size: rocket.fontSize))
+                        .position(rocket.position)
+                        .animation(nil, value: rocket.position)
+                }
+            }
+            .onAppear {
+                rockets = (0..<rocketCount).map { _ in
+                    randomRocket(in: geo.size)
+                }
+                Timer.scheduledTimer(withTimeInterval: 1/60, repeats: true) { _ in
+                    withAnimation(.linear(duration: 1/60)) {
+                        updateRockets(in: geo.size)
+                    }
+                }
+            }
+        }
+        .allowsHitTesting(false)
+    }
+
+    private func randomRocket(in size: CGSize) -> Rocket {
+        let fontSize = fontSizes.randomElement()!
+        let speed = CGFloat.random(in: 2...5)
+        let startFromLeft = Bool.random()
+        let x: CGFloat = startFromLeft ? -fontSize : CGFloat.random(in: 0...size.width)
+        let y: CGFloat = startFromLeft ? CGFloat.random(in: 0...size.height) : size.height + fontSize
+        return Rocket(position: CGPoint(x: x, y: y), fontSize: fontSize, speed: speed)
+    }
+
+    private func updateRockets(in size: CGSize) {
+        for i in rockets.indices {
+            var rocket = rockets[i]
+            rocket.position.x += rocket.speed
+            rocket.position.y -= rocket.speed
+            if rocket.position.x > size.width + rocket.fontSize || rocket.position.y < -rocket.fontSize {
+                rockets[i] = randomRocket(in: size)
+            } else {
+                rockets[i] = rocket
+            }
+        }
+    }
+}
+
+#Preview {
+    StartScreenSceneView()
+        .frame(width: 1920, height: 1080)
 }
